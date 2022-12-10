731f58e515570fd9ac38eed2f6a7cb383d2f5807
Thomas Nixon <tom@tomn.co.uk>
2022-12-10 18:25:41 +0000

    nix: add yarp

--- flake.nix
+++ flake.nix
@@ -3,14 +3,29 @@
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
+
+    yarp_src.url = "github:tomjnixon/yarp/time_window";
+    yarp_src.flake = false;
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, yarp_src }:
   utils.lib.eachSystem utils.lib.defaultSystems (system:
     let
       pkgs = nixpkgs.legacyPackages."${system}";
       python = pkgs.python3;
     in rec {
+      packages.yarp = python.pkgs.buildPythonPackage rec {
+        name = "yarp";
+        format = "setuptools";
+        src = yarp_src;
+        propagatedBuildInputs = with python.pkgs; [
+          sentinel
+        ];
+        checkInputs = with python.pkgs; [ pytest pytest-asyncio mock ];
+        checkPhase = "pytest";
+        pythonImportsCheck = [ "yarp" ];
+      };
+
       packages.eshet_py = python.pkgs.buildPythonPackage rec {
         name = "eshet";
         format = "flit";
@@ -18,6 +33,7 @@
         propagatedBuildInputs = with python.pkgs; [
           msgpack
           sentinel
+          packages.yarp
         ];
         pythonImportsCheck = [ "eshet" ];
       };
