c3dbbb7f516b548948d8f069639a28ea92d7baa6
Thomas Nixon <tom@tomn.co.uk>
2023-06-27 00:50:25 +0100

    run checks in nix

--- flake.nix
+++ flake.nix
@@ -26,6 +26,17 @@
         pythonImportsCheck = [ "yarp" ];
       };
 
+      packages.asyncio-time-travel = python.pkgs.buildPythonPackage rec {
+        pname = "asyncio-time-travel";
+        version = "0.3.0";
+        format = "setuptools";
+        src = python.pkgs.fetchPypi {
+          pname = "asyncio_time_travel";
+          inherit version;
+          sha256 = "sha256-WYmzizQEbocqz5YGmClbC1BTDVWvhhqvJfzilklvniM=";
+        };
+      };
+
       packages.eshet_py = python.pkgs.buildPythonPackage rec {
         name = "eshet";
         format = "flit";
@@ -35,6 +46,8 @@
           sentinel
           packages.yarp
         ];
+        nativeCheckInputs = with python.pkgs; [ pytest pytest-asyncio packages.asyncio-time-travel ];
+        checkPhase = "pytest -m 'not needs_server'";
         pythonImportsCheck = [ "eshet" ];
       };
 
